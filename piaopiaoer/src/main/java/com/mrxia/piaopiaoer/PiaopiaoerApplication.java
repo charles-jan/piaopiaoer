0ddcad802749f1aa114a2ec0e7056f380ac06969
xiazijian <521091041@qq.com>
2017-12-09 21:36:03 +0800

    初始化项目

---
+++ piaopiaoer/src/main/java/com/mrxia/piaopiaoer/PiaopiaoerApplication.java
@@ -0,0 +1,12 @@
+package com.mrxia.piaopiaoer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiaopiaoerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiaopiaoerApplication.class, args);
+	}
+}
