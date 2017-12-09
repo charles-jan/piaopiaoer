986642d7922ad3d2ee80625749233a981fa232ce
夏子健 <527091041@qq.com>
2017-12-09 22:13:29 +0800

    修改gitignore文件

--- src/main/java/com/mrxia/piaopiaoer/PiaopiaoerApplication.java
+++ src/main/java/com/mrxia/piaopiaoer/PiaopiaoerApplication.java
@@ -3,6 +3,9 @@ package com.mrxia.piaopiaoer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author xiazijian
+ */
 @SpringBootApplication
 public class PiaopiaoerApplication {
 
